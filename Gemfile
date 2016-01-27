0e0066313c5b890f7af897ee038ec7c8a314f604
David Wilbanks <dwilbank@gmail.com>
2016-01-27 11:42:23 -0800

    refactored team_controller

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'omniauth-facebook'#, '1.4.0'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'pry'
+  gem 'pry-nav'
   gem 'hirb'
   gem 'awesome_print'
 end
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -28,47 +28,23 @@ class TeamsController < ApplicationController
   end
 
   def add_monster
-
     team_id = params[:team_id].to_i
     monster_id = params[:monster_id].to_i
 
-    query = params_valid?(team_id, monster_id)
+    # result will be a hash containing either error messages, or the actual team and monster models
+    result = process_params(team_id, monster_id)
 
-    if query.valid?
-      monster.team_id = team_id
-      if monster.save
-        render json: {message:"added to team #{team.name}"}, status: 200
+    if result[:valid]
+
+      result[:monster].team_id = team_id
+      if result[:monster].save
+        render json: {message:"added to team #{result[:team].name}"}, status: 200
       else
-        render json: monster.errors, status: 422
+        render json: result[:monster].errors, status: 422
       end
-    end
 
-    # if !valid_monster_id(monster_id)
-    #   render json: {message:"That is not a valid monster"}, status:403
-    #   return
-    # elsif !valid_team_id(team_id)
-    #   render json: {message:"That is not a valid team"}, status:403
-    #   return
-    # else
-    #   true
-    # end
-
-    # team = Team.find_by_id(team_id)
-    # monster = Monster.find_by_id(monster_id)
-
-    if !team_belongs_to_user(team)
-      render json: {message:"That is not your team"}, status:403
-    elsif !monster_belongs_to_user(monster)
-      render json: {message:"That is not your monster"}, status:403
-    elsif !team_has_space(team)
-      render json: {message:"That team already has #{MAX_MONSTER_COUNT_PER_TEAM} monsters."}, status: 403
     else
-      # monster.team_id = team_id
-      # if monster.save
-      #   render json: {message:"added to team #{team.name}"}, status: 200
-      # else
-      #   render json: monster.errors, status: 422
-      # end
+        render json: {message:"#{result[:message]}"}, status: 422
     end
 
   end
@@ -77,37 +53,25 @@ class TeamsController < ApplicationController
     team_id = params[:team_id].to_i
     monster_id = params[:monster_id].to_i
 
-    if !valid_monster_id(monster_id)
-      render json: {message:"That is not a valid monster"}, status:403
-      return
-    elsif !valid_team_id(team_id)
-      render json: {message:"That is not a valid team"}, status:403
-      return
-    else
-      true
-    end
+    # result will be a hash containing either error messages, or the actual team and monster models
+    result = process_params(team_id, monster_id)
 
-    team = Team.find_by_id(team_id)
-    monster = Monster.find_by_id(monster_id)
+    if result[:valid]
 
-
-    if !team_belongs_to_user(team)
-      render json: {message:"That is not your team"}, status:403
-      return
-    elsif !monster_belongs_to_user(monster)
-      render json: {message:"That is not your monster"}, status:403
-      return
-    else
-      monster.team_id = nil
-      if monster.save
-        render json: {message:"removed from #{team.name}"}, status: 200
+      result[:monster].team_id = nil
+      if result[:monster].save
+        render json: {message:"removed from team #{result[:team].name}"}, status: 200
       else
-        render json: monster.errors, status: 422
+        render json: result[:monster].errors, status: 422
       end
+
+    else
+      render json: {message:"#{result[:message]}"}, status: 422
     end
   end
 
 
+  # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
   private
 
@@ -115,19 +79,40 @@ class TeamsController < ApplicationController
     params.require(:team).permit(:name)
   end
 
-  def params_valid?(team_id, monster_id)
-    query = {}
+  def process_params(team_id, monster_id)
+    result = {}
+    result[:status] = 403   # unless params are legit
+    result[:valid] = false  # unless params are legit
+    result[:team] = nil     # unless params are legit
+    result[:monster] = nil  # unless params are legit
+
     if !valid_monster_id(monster_id)
-      query.message = "That is not a valid monster"
-      query.status = 403
-      query.valid? = false
+      result[:message] = "That is not a valid monster"
     elsif !valid_team_id(team_id)
-      query.message = "That is not a valid team"
-      query.status = 403
-      query.valid? = false
+      result[:message] = "That is not a valid team"
     else
-      query.valid? = true
+
+      team = Team.find_by_id(team_id)
+      monster = Monster.find_by_id(monster_id)
+
+      # necessary because if we are REMOVING monsters,
+      # we don't want to check how many monsters are in a team (line 106)
+      calling_method = caller_locations(1,1)[0].label
+
+      if !team_belongs_to_user(team)
+        result[:message] = "That is not your team"
+      elsif !monster_belongs_to_user(monster)
+        result[:message] = "That is not your monster"
+      elsif !team_has_space(team) && calling_method == "add_monster"
+        result[:message] ="That team already has #{MAX_MONSTER_COUNT_PER_TEAM} monsters."
+      else
+        result[:status] = 200
+        result[:valid] = true
+        result[:team] = team
+        result[:monster] = monster
+      end
     end
+    return result
   end
 
   def valid_monster_id(monster_id)
@@ -155,11 +140,7 @@ class TeamsController < ApplicationController
     return team.monsters.count >= MAX_MONSTER_COUNT_PER_TEAM ? false : true
   end
 
-  ###################################################################
-
-  protected
-
-  def get_user
+    def get_user
     token = request
                 .headers["Authorization"]
                 .split('=')
